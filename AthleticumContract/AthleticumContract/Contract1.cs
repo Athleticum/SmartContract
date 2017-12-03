eea6ff51da4838223eb342d1d909a9a090d635b2
MaximeDenuit <MaximeDenuit@users.noreply.github.com>
2017-12-03 17:24:41 +0100

    StartProject

---
+++ AthleticumContract/AthleticumContract/Contract1.cs
@@ -0,0 +1,15 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace AthleticumContract
+{
+    public class Contract1 : Framework.SmartContract
+    {
+        public static void Main()
+        {
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+        }
+    }
+}
